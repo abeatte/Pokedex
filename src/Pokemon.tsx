56b69d23f70b7f22335aa3af4548cc29a17944e7
Art Beatte <abeatte@gmail.com>
2026-02-08 14:28:46 -0800

    Display species and image information for a pokemon

--- src/Pokemon.tsx
+++ src/Pokemon.tsx
@@ -1,10 +1,17 @@
 import type { Pokemon } from "./graphql/getAllPokemon";
 
-function pokemon(props: {pokemon: Pokemon}) {
-    const {pokemon} = props;
-    
+function pokemon(props: { pokemon: Pokemon }) {
+    const { pokemon } = props;
+
+    const species = pokemon.species;
+    const image = pokemon.sprite;
+
     return (
-        <div>{JSON.stringify(pokemon)}</div>
+        <>
+            <div>Species: ${species}</div>
+            <div>Image: <img src={image} /></div>
+            <div>{JSON.stringify(pokemon)}</div>
+        </>
     )
 }
 
