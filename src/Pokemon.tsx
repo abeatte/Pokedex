1f55b193d99075985e43686ce5ee8a8c4056384a
Art Beatte <abeatte@gmail.com>
2026-02-10 20:19:29 -0800

    Style Pokemon cards

--- src/Pokemon.tsx
+++ src/Pokemon.tsx
@@ -1,7 +1,7 @@
 import { FaPlay } from "react-icons/fa";
 import type { Pokemon } from "./graphql/getAllPokemon";
 import './Pokemon.css'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function pokemon(props: { pokemon: Pokemon }) {
     const { pokemon } = props;
@@ -19,6 +19,23 @@ function pokemon(props: { pokemon: Pokemon }) {
     } = pokemon;
 
     const [spriteClicked, setSpriteClicked] = useState(false);
+    const [showShiny, setShowShiny] = useState(false);
+    const [isFading, setIsFading] = useState(false);
+
+    useEffect(() => {
+        // Only animate if both regular and shiny sprites exist
+        if (!sprite || !shinySprite) return;
+
+        const interval = setInterval(() => {
+            setIsFading(true);
+            setTimeout(() => {
+                setShowShiny(prev => !prev);
+                setIsFading(false);
+            }, 600); // Match fade-out duration
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [sprite, shinySprite]);
 
     const playCry = () => {
         new Audio(cry)
@@ -28,26 +45,27 @@ function pokemon(props: { pokemon: Pokemon }) {
 
     return (
         <div className="card">
-            <div>{species}</div>
-            <div>
+            <div className="name">{species}</div>
+            <div className="image_window">
                 <span className="pokemon_image_container" onClick={() => setSpriteClicked(prev => !prev)} >
-                    <img src={spriteClicked ? backSprite : sprite} />
-                    <img src={spriteClicked ? shinyBackSprite : shinySprite} />
+                    <img className={isFading ? 'fading' : ''} src={showShiny ? (spriteClicked ? shinyBackSprite : shinySprite) : (spriteClicked ? backSprite : sprite)} />
                 </span>
-            </div>
-            <button onClick={playCry}>
+                <button className="play_button" onClick={playCry}>
                 <FaPlay />
             </button>
-            <div>Height: {height}m</div>
-            <div>Weight: {weight}kg</div>
-            <div>Number: {num}</div>
-            <div>Types: {Array.from(types.map(type => type.name)).join(', ')}</div>
-            {mythical || legendary && (<div>
-                {mythical && (<span>Mythical: {mythical}</span>)}
-                {legendary && (<span>Legendary: {legendary}</span>)}
-            </div>)}
-            <div>Catch Rate: {catchRate.base} | {catchRate.percentageWithOrdinaryPokeballAtFullHealth}</div>
-            <div>Hatch Time: {minimumHatchTime} steps - {maximumHatchTime} steps</div>
+            </div>
+            <div className="stats">
+                <div>Height: {height}m</div>
+                <div>Weight: {weight}kg</div>
+                <div>Number: {num}</div>
+                <div>Types: {Array.from(types.map(type => type.name)).join(', ')}</div>
+                {mythical || legendary && (<div>
+                    {mythical && (<span>Mythical: {mythical}</span>)}
+                    {legendary && (<span>Legendary: {legendary}</span>)}
+                </div>)}
+                <div>Catch Rate: {catchRate.base} | {catchRate.percentageWithOrdinaryPokeballAtFullHealth}</div>
+                <div>Hatch Time: {minimumHatchTime} steps - {maximumHatchTime} steps</div>
+            </div>
         </div>
     );
 }
